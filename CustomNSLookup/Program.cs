1df30efa03f9b1bc5f46203e09440246067fdc76
jodaut <josedavidut64@gmail.com>
2021-12-21 22:16:50 -0600

    Add project files.

---
+++ CustomNSLookup/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace CustomNSLookup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress[] ipAddresses = new IPAddress[args.Length];
+            foreach (string url in args)
+            {
+                try
+                {
+                    IPHostEntry host = Dns.GetHostEntry(url);
+                    IPAddress[] ipAddreses = host.AddressList;
+                    Console.WriteLine("HOST: {0}", host.HostName);
+                    Console.WriteLine("IP address:");
+                    foreach (IPAddress ip in ipAddreses)
+                    {
+                        Console.WriteLine(ip);
+                    }
+                    
+                }
+                catch(System.Net.Sockets.SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+    }
+}
